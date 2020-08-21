1c196ccc1b88fe6fc0821ca0e5e19e686998bd71
Viktor Farcic <vfarcic@farcic.com>
2020-08-21 02:08:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,90 @@
+# Google Cloud Kubernetes Engine (GKE) Cluster With Terraform And CodeFresh
+
+## Preparing
+
+```bash
+open https://github.com/vfarcic/cf-terraform-gke
+
+# Fork it
+
+cd cf-terraform-gke
+
+cp orig/*.tf .
+
+gcloud auth application-default login
+
+export PROJECT_ID=doc-$(date +%Y%m%d%H%M%S)
+
+gcloud projects create $PROJECT_ID
+
+gcloud iam service-accounts \
+    create devops-catalog \
+    --project $PROJECT_ID \
+    --display-name devops-catalog
+
+gcloud iam service-accounts \
+    keys create account.json \
+    --iam-account devops-catalog@$PROJECT_ID.iam.gserviceaccount.com \
+    --project $PROJECT_ID
+
+gcloud projects \
+    add-iam-policy-binding $PROJECT_ID \
+    --member serviceAccount:devops-catalog@$PROJECT_ID.iam.gserviceaccount.com \
+    --role roles/owner
+
+export BUCKET_NAME=doc-$(date +%Y%m%d%H%M%S)
+
+export REGION=us-east1
+
+open https://console.cloud.google.com/billing/linkedaccount?project=$PROJECT_ID
+
+gsutil mb \
+    -p $PROJECT_ID \
+    -l $REGION \
+    -c "NEARLINE" \
+    gs://$BUCKET_NAME
+
+cat variables.tf
+
+gcloud container get-server-config \
+    --region $REGION
+
+export VERSION=[...]
+
+cat variables.tf \
+    | sed -e "s@CHANGE_PROJECT_ID@$PROJECT_ID@g" \
+    | sed -e "s@CHANGE_VERSION@$VERSION@g" \
+    | tee variables.tf
+
+cat main.tf
+
+cat main.tf \
+    | sed -e "s@CHANGE_BUCKET@$BUCKET_NAME@g" \
+    | tee main.tf
+
+cat output.tf
+
+git add .
+
+git commit -m "Initial commit"
+
+git push
+```
+
+## Creating Manually
+
+```bash
+terraform init
+
+terraform apply
+```
+
+## Destroy Manually
+
+```bash
+# TODO: Change variables
+
+terraform apply
+
+gcloud projects delete $PROJECT_ID
+```
---
+++ main.tf
@@ -0,0 +1,79 @@
+provider "google" {
+  credentials = file("account.json")
+  project     = var.project_id
+  region      = var.region
+}
+
+terraform {
+  backend "gcs" {
+    bucket      = "CHANGE_BUCKET"
+    prefix      = "terraform/state"
+    credentials = "account.json"
+  }
+}
+
+resource "google_project_service" "container" {
+  service = "container.googleapis.com"
+}
+
+resource "google_container_cluster" "primary" {
+  name                     = var.cluster_name
+  location                 = var.region
+  min_master_version       = var.k8s_version
+  remove_default_node_pool = true
+  initial_node_count       = 1
+  depends_on = [
+    google_project_service.container
+  ]
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  name               = var.cluster_name
+  location           = var.region
+  cluster            = google_container_cluster.primary.name
+  version            = var.k8s_version
+  initial_node_count = var.min_node_count
+  node_config {
+    preemptible  = var.preemptible
+    machine_type = var.machine_type
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+  autoscaling { 
+    min_node_count = var.min_node_count
+    max_node_count = var.max_node_count
+  }
+  management {
+    auto_upgrade = false
+  }
+  timeouts {
+    create = "15m"
+    update = "1h"
+  }
+}
+
+resource "null_resource" "kubeconfig" {
+  provisioner "local-exec" {
+    command = "KUBECONFIG=$PWD/kubeconfig gcloud container clusters get-credentials ${var.cluster_name} --project ${var.project_id} --region ${var.region}"
+  }
+  depends_on = [
+    google_container_cluster.primary,
+  ]
+}
+
+resource "null_resource" "destroy-kubeconfig" {
+  provisioner "local-exec" {
+    when    = destroy
+    command = "rm -f $PWD/kubeconfig"
+  }
+}
+
+resource "null_resource" "ingress-nginx" {
+  provisioner "local-exec" {
+    command = "KUBECONFIG=$PWD/kubeconfig kubectl apply --filename https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/cloud/deploy.yaml"
+  }
+  depends_on = [
+    null_resource.kubeconfig,
+  ]
+}
